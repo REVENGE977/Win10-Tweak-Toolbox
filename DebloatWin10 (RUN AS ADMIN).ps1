9e2771a0f8135fec10a1d2b9b119062ba0077138
REVENGE <32039975+REVENGE977@users.noreply.github.com>
2021-03-19 13:15:30 +0300

    yes

---
+++ DebloatWin10 (RUN AS ADMIN).ps1
@@ -0,0 +1,367 @@
+﻿#Requires -RunAsAdministrator
+
+### Windows 10 Tweak Toolbox
+
+Write-Output "############ Windows Tweak Toolbox ############"
+Write-Output "# TOOLBOX BY      :: REVENGE                  #"
+Write-Output "# TOOLBOX VERSION :: v0.1                     #"
+Write-Output "############ Windows Tweak Toolbox ############"
+
+Function YesNo($text) {
+    do {
+        $response = Read-Host -Prompt $text
+        if ($response -eq 'y') {
+            return $true;
+            break
+        }
+    } until ($response -eq 'n')
+}
+
+# use at your own risk prompt
+if(!(YesNo -text "Do you agree to use this toolbox at your own risk ? [Y/N]")) {
+    exit
+}
+
+# creating a restore point
+if(YesNo -text "Do you want to create a restore point ? [Y/N]") {
+    Enable-ComputerRestore -Drive "C:\"
+    Checkpoint-Computer -Description "RestorePoint1" -RestorePointType "MODIFY_SETTINGS"
+    Write-Output "[ INFO ] Restore point created !"
+}
+
+# disabling windows update
+if(YesNo -text "Do you want to disable windows updates ? [Y/N]") {
+    Set-Service -Name wuauserv -StartupType Disabled
+    Set-Service -Name TrustedInstaller -StartupType Disabled
+    Stop-Service -Name WaaSMedicSvc -Force
+    Stop-Service -Name wuauserv -Force
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "NoAutoRebootWithLoggedOnUsers" -Type DWord -Value 1
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate\AU" -Name "AUPowerManagement" -Type DWord -Value 0
+    Write-Output "[ INFO ] Disabled windows updates"
+};
+
+# removing bloatware
+if(YesNo -text "Do you want to remove bloatware ? [Y/N]") {
+        $apps = @(
+        "Microsoft.3DBuilder"
+        "Microsoft.Microsoft3DViewer"
+        "Microsoft.Appconnector"
+        "Microsoft.BingFinance"
+        "Microsoft.BingNews"
+        "Microsoft.BingSports"
+        "Microsoft.BingWeather"
+        "Microsoft.WindowsCamera"
+        "Microsoft.Getstarted"
+        "Microsoft.MicrosoftOfficeHub"
+        "Microsoft.MicrosoftSolitaireCollection"
+        "Microsoft.MicrosoftStickyNotes"
+        "Microsoft.Office.OneNote"
+        "Microsoft.OneConnect"
+        "Microsoft.People"
+        "Microsoft.SkypeApp"
+        "Microsoft.StorePurchaseApp"
+        "Microsoft.WindowsAlarms"
+        "Microsoft.Windows.Cortana"
+        "Microsoft.WindowsMaps"
+        "Microsoft.WindowsPhone"
+        "Microsoft.WindowsSoundRecorder"
+        "Microsoft.XboxApp"
+        "Microsoft.XboxGameOverlay"
+        "Microsoft.XboxSpeechToTextOverlay"
+        "Microsoft.XboxIdentityProvider"
+        "Microsoft.XboxGameXCallableUI"
+        "Microsoft.ZuneMusic"
+        "Microsoft.ZuneVideo"
+        "microsoft.windowscommunicationsapps"
+        "Microsoft.Wallet"
+        "Microsoft.MinecraftUWP"
+        "Microsoft.NetworkSpeedTest"
+        "Microsoft.PPIProjection"
+        "Microsoft.MicrosoftEdge"
+        "Microsoft.MicrosoftPowerBIForWindows"
+        "AdobeSystemsIncorporated.AdobePhotoshopExpress"
+        "Microsoft.CommsPhone"
+        "Microsoft.ConnectivityStore"
+        "Microsoft.Messaging"
+        "Microsoft.Office.Sway"
+        "Microsoft.OneConnect"
+        "Microsoft.WindowsFeedbackHub"
+        "Microsoft.BingFoodAndDrink"
+        "Microsoft.BingTravel"
+        "Microsoft.BingHealthAndFitness"
+        "Microsoft.WindowsReadingList"
+        "CortanaListenUIApp"
+        "Microsoft.MicrosoftSolitaiteCollection"
+        "CortanaListenUIApp_10.0.15063.0_neutral__cw5n1h2txyewy"
+        "9E2F88E3.Twitter"
+        "PandoraMediaInc.29680B314EFC2"
+        "Flipboard.Flipboard"
+        "ShazamEntertainmentLtd.Shazam"
+        "king.com.CandyCrushSaga"
+        "king.com.CandyCrushSodaSaga"
+        "king.com.*"
+        "ClearChannelRadioDigital.iHeartRadio"
+        "4DF9E0F8.Netflix"
+        "6Wunderkinder.Wunderlist"
+        "Drawboard.DrawboardPDF"
+        "2FE3CB00.PicsArt-PhotoStudio"
+        "D52A8D61.FarmVille2CountryEscape"
+        "TuneIn.TuneInRadio"
+        "GAMELOFTSA.Asphalt8Airborne"
+        "TheNewYorkTimes.NYTCrossword"
+        "DB6EA5DB.CyberLinkMediaSuiteEssentials"
+        "Facebook.Facebook"
+        "flaregamesGmbH.RoyalRevolt2"
+        "Playtika.CaesarsSlotsFreeCasino"
+        "46928bounde.EclipseManager_2.2.4.51_neutral__a5h4egax66k6y"
+        "ActiproSoftwareLLC.562882FEEB491_2.6.18.18_neutral__24pqs290vpjk0"
+        "AdobeSystemsIncorporated.AdobePhotoshopExpress_1.3.2.4_x64__ynb6jyjzte8ga"
+        "ActiproSoftwareLLC.562882FEEB491"
+    );
+
+    ForEach ($app in $apps) {
+        Write-Output "[ INFO ] Trying to remove $app"
+    
+        Get-AppxPackage -Name $app -AllUsers | Remove-AppxPackage
+    
+        Get-AppXProvisionedPackage -Online |
+            Where-Object DisplayName -EQ $app |
+            Remove-AppxProvisionedPackage -Online 
+    }
+
+    Write-Output "[ INFO ] Most windows 10 bloatware removed"
+}
+
+# disabling background apps
+if(YesNo -text "Do you want to disable background apps ? [Y/N]") {
+	Get-ChildItem -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\BackgroundAccessApplications" -Exclude "Microsoft.Windows.Cortana*" | ForEach {
+		Set-ItemProperty -Path $_.PsPath -Name "Disabled" -Type DWord -Value 1
+		Set-ItemProperty -Path $_.PsPath -Name "DisabledByUser" -Type DWord -Value 1
+	}
+	Write-Output "[ INFO ] Disabled background apps"
+}
+
+# disabling windows search service
+if(YesNo -text "Do you want to disable windows search indexer (this will make windows search faster !) ? [Y/N]") {
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "BingSearchEnabled" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Search" -Name "CortanaConsent" -Type DWord -Value 0
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\Windows Search" -Name "DisableWebSearch" -Type DWord -Value 1
+	Stop-Service "WSearch" -WarningAction SilentlyContinue
+	Set-Service "WSearch" -StartupType Disabled
+    Write-Output "[ INFO ] Disabled windows search indexer !"
+}
+
+# Installing Chocolatey package manager
+if(YesNo -text "Do you want to install Chocolatey ? [Y/N]") {
+    Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+}
+
+# Using O&O Shutup
+if(YesNo -text "Do you want to download & run O&O Shutup ? [Y/N]") {
+    Import-Module BitsTransfer		
+	Start-BitsTransfer -Source "https://pastebin.com/raw/CWhzpKaC" -Destination ooshutup10.cfg
+	Start-BitsTransfer -Source "https://dl5.oo-software.com/files/ooshutup10/OOSU10.exe" -Destination OOSU10.exe
+	Start-Sleep -s 2
+	./OOSU10 ooshutup10.cfg /quiet
+	
+	Start-Sleep -s 5
+    Remove-Item '.\OOSU10'
+    Remove-Item '.\ooshutup10.cfg'	
+
+    Write-Output "[ INFO ] Used O&O Shutup"
+}
+
+# Disabling telemetry 
+if(YesNo -text "Do you want to disable telemetry ? [Y/N]") {
+    $DataCollection1 = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection"
+    $DataCollection2 = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DataCollection"
+    $DataCollection3 = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Policies\DataCollection"    
+    If (Test-Path $DataCollection1) {
+        Set-ItemProperty $DataCollection1  AllowTelemetry -Value 0 
+    }
+    If (Test-Path $DataCollection2) {
+        Set-ItemProperty $DataCollection2  AllowTelemetry -Value 0 
+    }
+    If (Test-Path $DataCollection3) {
+        Set-ItemProperty $DataCollection3  AllowTelemetry -Value 0 
+    }
+
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\Microsoft Compatibility Appraiser" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Application Experience\ProgramDataUpdater" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Autochk\Proxy" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Customer Experience Improvement Program\Consolidator" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\Customer Experience Improvement Program\UsbCeip" | Out-Null
+	Disable-ScheduledTask -TaskName "Microsoft\Windows\DiskDiagnostic\Microsoft-Windows-DiskDiagnosticDataCollector" | Out-Null
+
+    Write-Output "[ INFO ] Disabled telemetry"
+}
+
+# Disabling location tracking 
+if(YesNo -text "Do you want to disable location tracking ? [Y/N]") {
+    $SensorState = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Sensor\Overrides\{BFA794E4-F964-4FDB-90F6-51056BFE4B44}"
+    $LocationConfig = "HKLM:\SYSTEM\CurrentControlSet\Services\lfsvc\Service\Configuration"
+    If (!(Test-Path $SensorState)) {
+        New-Item $SensorState
+    }
+    Set-ItemProperty $SensorState SensorPermissionState -Value 0 
+    If (!(Test-Path $LocationConfig)) {
+        New-Item $LocationConfig
+    }
+    Set-ItemProperty $LocationConfig Status -Value 0
+
+    Write-Output "[ INFO ] Disabled location tracking"
+}
+
+# Disabling scheduled tasks
+if(YesNo -text "Do you want to disable scheduled tasks ? [Y/N]") {
+    Get-ScheduledTask  XblGameSaveTask | Disable-ScheduledTask
+    Get-ScheduledTask  Consolidator | Disable-ScheduledTask
+    Get-ScheduledTask  UsbCeip | Disable-ScheduledTask
+    Get-ScheduledTask  DmClient | Disable-ScheduledTask
+    Get-ScheduledTask  DmClientOnScenarioDownload | Disable-ScheduledTask
+
+    Write-Output "Stopping and disabling Diagnostics Tracking Service"
+
+    Stop-Service "DiagTrack"
+    Set-Service "DiagTrack" -StartupType Disabled
+
+    Write-Output "[ INFO ] Disabled scheduled tasks"
+}
+
+# Disabling cortana
+if(YesNo -text "Do you want to disable cortana ? [Y/N]") {
+    $Cortana1 = "HKCU:\SOFTWARE\Microsoft\Personalization\Settings"
+    $Cortana2 = "HKCU:\SOFTWARE\Microsoft\InputPersonalization"
+    $Cortana3 = "HKCU:\SOFTWARE\Microsoft\InputPersonalization\TrainedDataStore"
+    If (!(Test-Path $Cortana1)) {
+        New-Item $Cortana1
+    }
+    Set-ItemProperty $Cortana1 AcceptedPrivacyPolicy -Value 0 
+    If (!(Test-Path $Cortana2)) {
+        New-Item $Cortana2
+    }
+    Set-ItemProperty $Cortana2 RestrictImplicitTextCollection -Value 1 
+    Set-ItemProperty $Cortana2 RestrictImplicitInkCollection -Value 1 
+    If (!(Test-Path $Cortana3)) {
+        New-Item $Cortana3
+    }
+    Set-ItemProperty $Cortana3 HarvestContacts -Value 0
+
+    Write-Output "[ INFO ] Disabled cortana"
+}
+
+# Disabling Aciton Center
+if(YesNo -text "Do you want to disable action center ? [Y/N]") {
+	If (!(Test-Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer")) {
+		New-Item -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" | Out-Null
+	}
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Policies\Microsoft\Windows\Explorer" -Name "DisableNotificationCenter" -Type DWord -Value 1
+	Set-ItemProperty -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\PushNotifications" -Name "ToastEnabled" -Type DWord -Value 0
+    Write-Output "[ INFO ] Disabled action center"
+}
+
+# Enabling visualfx
+if(YesNo -text "Do you want to change windows animations to performance ? [Y/N]") {
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "DragFullWindows" -Type String -Value 0
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "MenuShowDelay" -Type String -Value 200
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop" -Name "UserPreferencesMask" -Type Binary -Value ([byte[]](144,18,3,128,16,0,0,0))
+	Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\WindowMetrics" -Name "MinAnimate" -Type String -Value 0
+	Set-ItemProperty -Path "HKCU:\Control Panel\Keyboard" -Name "KeyboardDelay" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewAlphaSelect" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "ListviewShadow" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAnimations" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\VisualEffects" -Name "VisualFXSetting" -Type DWord -Value 3
+	Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\DWM" -Name "EnableAeroPeek" -Type DWord -Value 0
+    Write-Output "[ INFO ] Animation settings has changed"
+}
+
+# Uninstalling OneDrive
+if(YesNo -text "Do you want to uninstall OneDrive ? [Y/N]") {
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows\OneDrive" -Name "DisableFileSyncNGSC" -Type DWord -Value 1
+	Stop-Process -Name "OneDrive" -ErrorAction SilentlyContinue
+	Start-Sleep -s 2
+	$onedrive = "$env:SYSTEMROOT\SysWOW64\OneDriveSetup.exe"
+	If (!(Test-Path $onedrive)) {
+		$onedrive = "$env:SYSTEMROOT\System32\OneDriveSetup.exe"
+	}
+	Start-Process $onedrive "/uninstall" -NoNewWindow -Wait
+	Start-Sleep -s 2
+	Stop-Process -Name "explorer" -ErrorAction SilentlyContinue
+	Start-Sleep -s 2
+	Remove-Item -Path "$env:USERPROFILE\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "$env:LOCALAPPDATA\Microsoft\OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "$env:PROGRAMDATA\Microsoft OneDrive" -Force -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "$env:SYSTEMDRIVE\OneDriveTemp" -Force -Recurse -ErrorAction SilentlyContinue
+	If (!(Test-Path "HKCR:")) {
+		New-PSDrive -Name HKCR -PSProvider Registry -Root HKEY_CLASSES_ROOT | Out-Null
+	}
+	Remove-Item -Path "HKCR:\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -ErrorAction SilentlyContinue
+	Remove-Item -Path "HKCR:\Wow6432Node\CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}" -Recurse -ErrorAction SilentlyContinue
+}
+
+# Disabling Windows Defender
+if(YesNo -text "Do you want to disable Windows Security ? [Y/N]") {
+    Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "ConsentPromptBehaviorAdmin" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System" -Name "PromptOnSecureDesktop" -Type DWord -Value 0
+    
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Type DWord -Value 1
+	If ([System.Environment]::OSVersion.Version.Build -eq 14393) {
+		Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "WindowsDefender" -ErrorAction SilentlyContinue
+	} ElseIf ([System.Environment]::OSVersion.Version.Build -ge 15063) {
+		Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" -Name "SecurityHealth" -ErrorAction SilentlyContinue
+	}
+
+    
+    If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Force | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Name "SpynetReporting" -Type DWord -Value 0
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Spynet" -Name "SubmitSamplesConsent" -Type DWord -Value 2
+    Write-Host "Disabling Meltdown (CVE-2017-5754) compatibility flag..."
+	Remove-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\QualityCompat" -Name "cadca5fe-87d3-4b96-b7fb-a231484277cc" -ErrorAction SilentlyContinue
+    Write-Host "Disabling Malicious Software Removal Tool offering..."
+	If (!(Test-Path "HKLM:\SOFTWARE\Policies\Microsoft\MRT")) {
+		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\MRT" | Out-Null
+	}
+	Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\MRT" -Name "DontOfferThroughWUAU" -Type DWord -Value 1
+	
+    sc config WinDefend start= disabled
+    sc stop WinDefend
+    Set-MpPreference -DisableRealtimeMonitoring $true
+
+    Write-Output "[ INFO ] Disabled Windows Defender"
+}
+
+# Downloading reduce memory
+if(YesNo -text "Do you want to download reduce memory ? [Y/N]") {
+	$DesktopPath = [Environment]::GetFolderPath("Desktop")
+	Start-BitsTransfer -Source "https://cdn.discordapp.com/attachments/809458069389836290/822154681962594314/ReduceMemory.exe" -Destination "$DesktopPath/ReduceMemory.exe"
+	Start-Process -FilePath "$DesktopPath\ReduceMemory.exe"
+    Write-Output "[ INFO ] downloaded ReduceMemory"
+}
+
+# Downloading reduce disk
+if(YesNo -text "Do you want to download reduce disk ? [Y/N]") {
+	$DesktopPath = [Environment]::GetFolderPath("Desktop")
+	Start-BitsTransfer -Source "https://cdn.discordapp.com/attachments/809458069389836290/818455759582527508/ReduceDisk.exe" -Destination "$DesktopPath/ReduceDisk.exe"
+	Start-Process -FilePath "$DesktopPath\ReduceDisk.exe"
+    Write-Output "[ INFO ] downloaded ReduceDisk"
+}
+
+Write-Output ""
+Write-Output "############ Windows Tweak Toolbox ############"
+Write-Output "- Please restart your machine to apply the changes !"
+Write-Output "- Thanks for using windows tweak toolbox, by REVENGE"
+Write-Output "############ Windows Tweak Toolbox ############"
